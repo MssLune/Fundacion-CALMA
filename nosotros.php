2140cd91111a447ea7395d148ca831daef14701b
parisaranda <63311897+parisaranda@users.noreply.github.com>
2021-04-23 12:21:31 -0500

    Add files via upload
    ops

---
+++ nosotros.php
@@ -0,0 +1,11 @@
+
+<?php include_once 'includes/header.php' ?>
+
+
+        <?php include_once 'includes/aliados.php' ?>
+
+        <?php include_once 'includes/voluntarios.php' ?>
+
+        <?php include_once 'includes/galeria.php' ?>
+
+<?php include_once 'includes/footer.php' ?>
