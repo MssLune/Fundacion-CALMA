f173afc6b5f0ccd2ab46a8339efc0734db5b8943
Geraldine J <56179689+MssLune@users.noreply.github.com>
2021-04-27 17:45:59 +0200

    equisde

--- includes/voluntario/aaaa.php
+++ includes/voluntario/aaaa.php
@@ -1,3 +1,7 @@
+<svg width="100%" height="30" >
+      <rect width="100%" height="100"
+      style="fill:rgba(246, 246, 248, 0.596);stroke-width:10;stroke:rgba(246, 246, 248, 0.596);" />
+    </svg>
 <section class="py-md-7 pb-5">
     <div class="container">
       <div class="row text-center">
@@ -95,6 +99,10 @@
   </section>
   <!-- -------- END Features w/ 4 infos - just colored icons -------- -->
   <!-- START Blogs w/ 3 images & quote & text -->
+
+  <hr class="dark my-md-6 mt-2 mx-7">
+
+
   <section>
     <div class="container">
       <div class="row">
@@ -105,29 +113,139 @@
           </p>
         </div>
       </div>
+    </div>
+  </section>
+
+
+
+
+ 
+  <section class="py-7">
+    <div class="container">
+      <div class="row">
+      </div>
       <div class="row mt-5">
-        <div class="col-md-6 position-relative">
-          
-          
-          <!-- Second image on the left side of the article -->
-          <div class="position-absolute bottom-0 mb-5 ms-n2 me-3 start-0 end-2 d-md-block d-none">
-            <div class="blur-shadow-image">
-              <img class="image-container rounded-3 img-fluid position-relative bg-cover" src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/coding.jpg">
+        <div class="col-md-4 ms-auto my-auto">
+          <a href="javascript:;">
+            <div class="card card-background tilt" data-tilt="">
+              <div class="full-background" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/team-working.jpg')"></div>
+              <div class="card-body pt-7 text-center">
+                <div class="icon icon-lg up mb-3">
+                  <svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <title>chart-pie-35</title>
+                    <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="Rounded-Icons" transform="translate(-1720.000000, -742.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                        <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)">
+                          <g id="chart-pie-35" transform="translate(4.000000, 451.000000)">
+                            <path d="M21.6666667,18.3333333 L39.915,18.3333333 C39.11,8.635 31.365,0.89 21.6666667,0.085 L21.6666667,18.3333333 Z" id="Path" opacity="0.602864583"></path>
+                            <path d="M20.69,21.6666667 L7.09833333,35.2583333 C10.585,38.21 15.085,40 20,40 C30.465,40 39.0633333,31.915 39.915,21.6666667 L20.69,21.6666667 Z" id="Path"></path>
+                            <path d="M18.3333333,19.31 L18.3333333,0.085 C8.085,0.936666667 0,9.535 0,20 C0,24.915 1.79,29.415 4.74166667,32.9016667 L18.3333333,19.31 Z" id="Path"></path>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+                <h1 class="text-white up mb-0">Search and Discover!</h1>
+                <p class="lead up">Website visitors</p>
+                <button type="button" class="btn btn-white btn-lg mt-3 up">Get Started</button>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-5 me-auto my-auto ms-md-5">
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>1. Listen to Social Conversations</h5>
+              <p>
+                Gain access to the demographics, psychographics, and location of unique people who are interested and talk about your brand.
+              </p>
+            </div>
+          </div>
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>2. Performance Analyze</h5>
+              <p>
+                Unify data from Facebook, Instagram, Twitter, LinkedIn, and Youtube to gain rich insights from easy-to-use reports.
+              </p>
+            </div>
+          </div>
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>3. Social Conversions</h5>
+              <p>
+                Track actions taken on your website that originated from social, and understand the impact on your bottom line.
+              </p>
             </div>
           </div>
         </div>
-        <div class="col-md-5">
-          <!-- First image on the right side, above the article -->
-          <h3 class="mb-4">So what does the new record for the lowest level of winter ice actually mean</h3>
-          <p>The Arctic Ocean freezes every winter and much of the sea-ice then thaws every summer, and that process will continue whatever happens with climate change. Even if the Arctic continues to be one of the fastest-warming regions of the world, it will always be plunged into bitterly cold polar dark every winter. And year-by-year, for all kinds of natural reasons, there’s huge variety of the state of the ice. For a start, it does not automatically follow that a record amount of ice will melt this summer
-          </p>
-          
+      </div>
+      <hr class="dark my-md-6 mt-2 mx-7">
+      <div class="row">
+        <div class="col-md-5 ms-auto my-auto">
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>1. Always In Sync</h5>
+              <p>
+                No matter where you are, Trello stays in sync across all of your devices.
+              </p>
+            </div>
+          </div>
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>2. Work With Any Team</h5>
+              <p>
+                Whether it’s for work or even the next family vacation, Trello helps your team.
+              </p>
+            </div>
+          </div>
+          <div class="p-3 info-horizontal d-flex">
+            <div>
+              <h5>3. A Productivity Platform</h5>
+              <p>
+                Integrate the apps your team already uses directly into your workflow.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 me-auto my-auto ms-md-5">
+          <a href="javascript:;">
+            <div class="card card-background tilt" data-tilt="">
+              <div class="full-background" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/coffee-shop.jpg')"></div>
+              <div class="card-body pt-7 text-center">
+                <div class="icon icon-lg up mb-3">
+                  <svg width="40px" height="40px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <title>customer-support</title>
+                    <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="Rounded-Icons" transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                        <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)">
+                          <g id="customer-support" transform="translate(1.000000, 0.000000)">
+                            <path d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z" id="Path" opacity="0.59858631"></path>
+                            <path d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z" id="Path"></path>
+                            <path d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z" id="Path"></path>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+                <h1 class="text-white up mb-0">Talk and Meet!</h1>
+                <p class="lead up">Social activities</p>
+                <button type="button" class="btn btn-white btn-lg mt-3 up">Get Started</button>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
   </section>
-  <!-- END Blogs w/ 3 images & quote & text -->
-  <section class="bg-gradient-primary position-relative overflow-hidden">
+
+
+
+
+
+   <!-- END Blogs w/ 3 images & quote & text -->
+   <section class="bg-gradient-primary position-relative overflow-hidden">
     <img src="../assets/img/shapes/waves-white.svg" alt="pattern-lines" class="position-absolute opacity-6">
     <div class="position-absolute w-100 z-inde-1 top-0 mt-n3">
       <svg width="100%" viewBox="0 -2 1920 157" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -269,134 +387,5 @@
           </g>
         </g>
       </svg>
-    </div>
-  </section>
-  <section class="py-7">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5 ms-auto me-auto text-center">
-          <div class="p-3 info-hover-warning">
-            <div class="icon icon-shape bg-gradient-primary shadow icon-shape-circle text-primary">
-              <i class="fa fa-cogs"></i>
-            </div>
-          </div>
-          <h3 class="text-gradient text-primary mb-0 mt-4">Conoce tus actividades</h3>
-          <h3>Encontraras muchas de ellas</h3>
-          <p>We’re constantly trying to express ourselves and actualize our dreams. Don't stop.</p>
-        </div>
-      </div>
-      <div class="row mt-5">
-        <div class="col-md-4 ms-auto my-auto">
-          <a href="javascript:;">
-            <div class="card card-background tilt" data-tilt="">
-              <div class="full-background" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/team-working.jpg')"></div>
-              <div class="card-body pt-7 text-center">
-                <div class="icon icon-lg up mb-3">
-                  <svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <title>chart-pie-35</title>
-                    <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <g id="Rounded-Icons" transform="translate(-1720.000000, -742.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                        <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)">
-                          <g id="chart-pie-35" transform="translate(4.000000, 451.000000)">
-                            <path d="M21.6666667,18.3333333 L39.915,18.3333333 C39.11,8.635 31.365,0.89 21.6666667,0.085 L21.6666667,18.3333333 Z" id="Path" opacity="0.602864583"></path>
-                            <path d="M20.69,21.6666667 L7.09833333,35.2583333 C10.585,38.21 15.085,40 20,40 C30.465,40 39.0633333,31.915 39.915,21.6666667 L20.69,21.6666667 Z" id="Path"></path>
-                            <path d="M18.3333333,19.31 L18.3333333,0.085 C8.085,0.936666667 0,9.535 0,20 C0,24.915 1.79,29.415 4.74166667,32.9016667 L18.3333333,19.31 Z" id="Path"></path>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-                <h1 class="text-white up mb-0">Search and Discover!</h1>
-                <p class="lead up">Website visitors</p>
-                <button type="button" class="btn btn-white btn-lg mt-3 up">Get Started</button>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-md-5 me-auto my-auto ms-md-5">
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>1. Listen to Social Conversations</h5>
-              <p>
-                Gain access to the demographics, psychographics, and location of unique people who are interested and talk about your brand.
-              </p>
-            </div>
-          </div>
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>2. Performance Analyze</h5>
-              <p>
-                Unify data from Facebook, Instagram, Twitter, LinkedIn, and Youtube to gain rich insights from easy-to-use reports.
-              </p>
-            </div>
-          </div>
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>3. Social Conversions</h5>
-              <p>
-                Track actions taken on your website that originated from social, and understand the impact on your bottom line.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <hr class="dark my-md-6 mt-2 mx-7">
-      <div class="row">
-        <div class="col-md-5 ms-auto my-auto">
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>1. Always In Sync</h5>
-              <p>
-                No matter where you are, Trello stays in sync across all of your devices.
-              </p>
-            </div>
-          </div>
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>2. Work With Any Team</h5>
-              <p>
-                Whether it’s for work or even the next family vacation, Trello helps your team.
-              </p>
-            </div>
-          </div>
-          <div class="p-3 info-horizontal d-flex">
-            <div>
-              <h5>3. A Productivity Platform</h5>
-              <p>
-                Integrate the apps your team already uses directly into your workflow.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 me-auto my-auto ms-md-5">
-          <a href="javascript:;">
-            <div class="card card-background tilt" data-tilt="">
-              <div class="full-background" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/coffee-shop.jpg')"></div>
-              <div class="card-body pt-7 text-center">
-                <div class="icon icon-lg up mb-3">
-                  <svg width="40px" height="40px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    <title>customer-support</title>
-                    <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <g id="Rounded-Icons" transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                        <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)">
-                          <g id="customer-support" transform="translate(1.000000, 0.000000)">
-                            <path d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z" id="Path" opacity="0.59858631"></path>
-                            <path d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z" id="Path"></path>
-                            <path d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z" id="Path"></path>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                </div>
-                <h1 class="text-white up mb-0">Talk and Meet!</h1>
-                <p class="lead up">Social activities</p>
-                <button type="button" class="btn btn-white btn-lg mt-3 up">Get Started</button>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
     </div>
   </section>
