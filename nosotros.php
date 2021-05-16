e379f0d5ad4b1a5da37d608f798fdd295824b976
july <julissa_2892@hotmail.com>
2021-05-16 14:12:04 -0500

    nosotros -comentar texto Julissa

--- nosotros.php
+++ nosotros.php
@@ -1,13 +1,13 @@
 <?php include_once 'includes/inicio/navbar.php' ?>
 <?php include_once 'includes/inicio/header.php' ?>
 
-        <?php include_once 'includes/nosotros/historia.php'Somos una organización a nivel mundial que promueve la paz y la mitigación de la violencia en todos sus escenarios. Promovemos el diálogo como herramienta principal para el logro de la paz. 
+        <?php include_once 'includes/nosotros/historia.php'//Somos una organización a nivel mundial que promueve la paz y la mitigación de la violencia en todos sus escenarios. Promovemos el diálogo como herramienta principal para el logro de la paz. 
 
-Estamos presentes en Perú, Costa Rica, México, Argentina, y Colombia.  ?>
+//Estamos presentes en Perú, Costa Rica, México, Argentina, y Colombia.  ?>
 
-        <?php include_once 'includes/nosotros/mision.php'Mitigación de la violencia en todos los escenarios de la población, para la instauración de una Cultura de Paz en el mundo. ?>
+        <?php include_once 'includes/nosotros/mision.php'//Mitigación de la violencia en todos los escenarios de la población, para la instauración de una Cultura de Paz en el mundo. ?>
 
-        <?php include_once 'includes/nosotros/aliados.php'Ser la fundación más reconocida a nivel mundial por promover la Cultura de Paz, siendo referente de las organizaciones en la participación del trabajo para erradicar la violencia. ?>
+        <?php include_once 'includes/nosotros/aliados.php'//Ser la fundación más reconocida a nivel mundial por promover la Cultura de Paz, siendo referente de las organizaciones en la participación del trabajo para erradicar la violencia. ?>
 
         <?php include_once 'includes/inicio/voluntarios.php' ?>
 
