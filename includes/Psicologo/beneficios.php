5fd12fc08477c312fecf650e483b3ea959a4eff9
ingridjane <ingrid.jane.parra@gmail.com>
2021-05-17 00:17:18 -0500

    video1

--- includes/Psicologo/beneficios.php
+++ includes/Psicologo/beneficios.php
@@ -8,12 +8,41 @@
         <h2 class="text-dark mb-4">PSICÓLOGO PRO BONO</h2>
         <!-- <p class="lead text-dark pe-md-4 me-md-4">
         </p> -->
-        <iframe width="100%" height="315"
-        src="https://www.youtube.com/embed/cv3cTM4T2DA">
-        </iframe>
-
-      </div>
+        
+        
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-pause="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+      <iframe class="first-slide"  width="100%" height="315"
+        src="https://www.youtube.com/embed/cv3cTM4T2DA"></iframe>
+        </iframe>
+      </div>
+      
+      <div class="carousel-item">
+      <iframe class="second-slide"  width="100%" height="315"
+      src="https://www.youtube.com/embed/Ou0P2A_RFIY" > </iframe>   
+            
+     </div>
+
+     <div class="carousel-item">
+      <iframe class="third-slide"  width="100%" height="315" src="https://www.youtube.com/embed/9EDI9D03540"></iframe>   
+     </div>
+       
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
   </div>
 </div>
-</div>
+
