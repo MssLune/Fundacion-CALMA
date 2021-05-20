29efeff87d076d7fdc9f2affb07e76e674e5475c
july <julissa_2892@hotmail.com>
2021-05-20 16:21:51 -0500

    referencias admin Julissa

--- includes/admin/index.php
+++ includes/admin/index.php
@@ -15,7 +15,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index.html" class="nav-link">Inicio</a>
+        <a href="consultoriocalma.php" class="nav-link">Inicio</a>
       </li>
     </ul>
 
@@ -145,7 +145,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Página Calma -->
-    <a href="../../index.php" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Ir a Calma</span>
     </a>
