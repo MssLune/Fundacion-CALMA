94926f6c73869b7e300b97fc39a946d449ead5c0
july <julissa_2892@hotmail.com>
2021-05-24 12:38:41 -0500

    adminsidebar Julissa

--- includes/admin/adminsidebar.php
+++ includes/admin/adminsidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Página Calma -->
     <a href="index.php" class="brand-link">
-      <img src="assets/img/logocalmaColor.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="assets/img/logocalmaColor.png" alt="AdminLTE Logo" class="brand-image logo-sidebar">
       <span class="brand-text font-weight-light">Ir a Calma</span>
     </a>
 
