1eb15b4bc28c93df6e41d9e468d73dee657d18f0
parisaranda <henry.aranda.b@gmail.com>
2021-06-03 10:39:06 -0500

    paris ejemplo

---
+++ adminusers.php
@@ -0,0 +1,10 @@
+<?php
+  ob_start();
+  @session_start();
+?>
+
+<?php include_once 'includes/admin/referenciasadmin.php'; ?>
+
+<?php include_once 'includes/admin/adminusers.php'; ?>
+
+<?php include_once 'includes/admin/adminscripts.php'; ?>
