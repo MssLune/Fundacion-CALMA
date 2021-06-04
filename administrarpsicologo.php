85d184f6c34dc9546fb3c4a9f50fb60d7aa35a72
NatiVic <natimayez09@gmail.com>
2021-06-04 11:57:42 -0500

    adminpsi nati

---
+++ administrarpsicologo.php
@@ -0,0 +1,11 @@
+<?php
+  ob_start();
+  @session_start();
+?>
+
+<?php include_once 'includes/admin/referenciasadmin.php'; ?>
+
+<?php include_once 'includes/admin/adminpsi.php'; ?>
+
+
+<?php include_once 'includes/admin/adminscripts.php'; ?>
