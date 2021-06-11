76b84f8f842733378465d2b4cedd9ca37735a364
Geraldine J <56179689+MssLune@users.noreply.github.com>
2021-06-11 19:37:23 +0200

    adminpsi

--- administrarpsicologo.php
+++ administrarpsicologo.php
@@ -5,7 +5,23 @@
 
 <?php include_once 'includes/admin/referenciasadmin.php'; ?>
 
+<?php include_once 'includes/admin/navbaradmin.php'; ?>
+
 <?php include_once 'includes/admin/adminpsi.php'; ?>
 
 
+
+    <?php include_once 'includes/admin/preloaderadmin.php'; ?>
+
+    <?php include_once 'includes/admin/adminsidebar.php'; ?>
+
+    <?php include_once 'includes/admin/controlsidebar.php'; ?>
+
+
+
+<?php include_once 'includes/admin/footeradmin.php'; ?>
+
 <?php include_once 'includes/admin/adminscripts.php'; ?>
+
+
+<script src="js/datatableFunctions.js"></script>
