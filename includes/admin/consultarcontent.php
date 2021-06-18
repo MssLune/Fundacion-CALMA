700443d5000656ed611b5518aae9cdcba967c986
ingridjane <ingrid.jane.parra@gmail.com>
2021-06-18 11:43:52 -0500

    progconsulta

--- includes/admin/consultarcontent.php
+++ includes/admin/consultarcontent.php
@@ -8,30 +8,16 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Programar consultas</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="consultoriocalma.php">Inicio</a></li>
-                                <li class="breadcrumb-item active">Programar consultas</li>
-                            </ol>
-                        </div>
-                    </div>
+          
+              <!-- general form elements -->
+              <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title h3-perfil">Programar consulta</h3>
                 </div>
-            </section>
-
-<!-- MAKE AN APPOINTMENT -->
-<section id="appointment" data-stellar-background-ratio="3">
-          <div class="container">
-               <div class="row">
-
-                    
-
-                    <div class="col-md-6 col-sm-6">
+                   
                          <!-- CONTACT FORM HERE -->
                          <form id="appointment-form" role="form" method="post" action="#">
-
-                            
+                           
 
                               <div class="wow fadeInUp" data-wow-delay="0.8s">
                                    <div class="col-md-6 col-sm-6">
@@ -63,8 +49,9 @@
                                         <input type="tel" class="form-control" id="phone" name="phone" placeholder="Escriba aqui">
                                         <label for="Message">Comentarios</label>
                                         <textarea class="form-control" rows="5" id="message" name="message" placeholder="Escriba aquí"></textarea>
-                                        <button type="submit" class="form-control" id="cf-submit" name="submit">Programar consulta</button>
-                                   </div>
+                                     <center>
+                                     <button type="submit" class="btn btn-primary">Programar consulta</button>
+                                     </center>                                   </div>
                               </div>
                         </form>
                     </div>
