875291a6c448d529c74750b39ecbf3a77b6ad123
july <julissa_2892@hotmail.com>
2021-06-21 15:12:18 -0500

    edit/delete admin Usuarios Psico Julissa

--- js/datatableFunctions.js
+++ js/datatableFunctions.js
@@ -47,3 +47,28 @@ $(document).ready(function () {
     "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
   }).buttons().container().appendTo('#tableAdminMedico_wrapper .col-md-6:eq(0)');
 });
+
+//Tabla para administrar usuarios
+$(document).ready(function () {
+  $("#tableAdminUser").DataTable({
+    //"responsive": true, 
+    "lengthChange": false, 
+    "autoWidth": false,
+    "scrollX": true,
+    "language": {
+      "lengthMenu": "Display _MENU_ records per page",
+      "zeroRecords": "Nada que ver. Lo sentimos.",
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "Sin datos disponibles.",
+      "infoFiltered": "(Filtrado de _MAX_ datos totales.)",
+      "paginate": {
+        "first":    'Pimero',
+        "previous": "Anterior",
+        "next": "Siguiente",
+        "last": "Último",
+      },
+      "search": "Buscar: "
+    },
+    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#tableAdminUser_wrapper .col-md-6:eq(0)');
+});
