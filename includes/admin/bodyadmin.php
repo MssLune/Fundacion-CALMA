d5a3f4197745513ce5c5b4a7c1700aeaabb0bcf5
july <julissa_2892@hotmail.com>
2021-06-25 12:04:57 -0500

    bodyadmin Julissa

--- includes/admin/bodyadmin.php
+++ includes/admin/bodyadmin.php
@@ -85,7 +85,7 @@
               //user
               echo '
 
-                  <div class="card w-75 mx-auto small-box pd25">
+                <div class="card w-75 mx-auto small-box pd25">
                   <div class="card-body">
                     <h5 class="card-title mg6 p-25">PROGRAMAR CONSULTA</h5>
                     <p class="card-text text-muted">Ingrese aquí para programar una consulta.</p>
@@ -93,41 +93,34 @@
                       <i class="ion ion-search"></i>
                     </div>
                     <a href="buscarConsulta.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
-                   
                   </div>
-                  </div>
-
-
+                </div>
 
                 <div class="card w-75 mx-auto small-box pd25">
                   <div class="card-body">
                     <h5 class="card-title mg6 p-25">MIS CONSULTAS</h5>
-                    <p class="card-text text-muted">Ingrese aquí para ver en una lista todas sus consultas realizadas.</p>
+                    <p class="card-text text-muted">Ingrese aquí para ver en una lista de todas sus consultas realizadas.</p>
                     <div class="icon">
                       <i class="ion ion-grid"></i>
                     </div>
                     <a href="tableConsultas.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
-                   
                   </div>
-                  </div>
-
-
+                </div>
               ';
             }else if(isset($_SESSION['Logueado']) && isset($_SESSION['privilegio']) && ($_SESSION['Logueado'] === true) && ($_SESSION['privilegio'] == 2)){
               //psicólogo
               echo '
                
                 <div class="card w-75 mx-auto small-box pd25 ">
-                <div class="card-body">
-                  <h5 class="card-title mg6 p-25">CONSULTAS ACEPTADAS</h5>
-                  <p class="card-text text-muted">Ingrese aquí para revisar todas sus consultas aceptadas.</p>
-                  <div class="icon">
-                      <i class="ion ion-grid"></i>
+                  <div class="card-body">
+                    <h5 class="card-title mg6 p-25">CONSULTAS ACEPTADAS</h5>
+                    <p class="card-text text-muted">Ingrese aquí para revisar todas sus consultas aceptadas.</p>
+                    <div class="icon">
+                        <i class="ion ion-grid"></i>
+                    </div>
+                    <a href="tableConsultas.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
-                  <a href="tableConsultas.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
-              </div>
-
               ';
             }else if(isset($_SESSION['Logueado']) && isset($_SESSION['privilegio']) && ($_SESSION['Logueado'] === true) && ($_SESSION['privilegio'] == 1)){
               //admin
@@ -140,14 +133,14 @@
                     <div class="icon">
                         <i class="ion ion-grid"></i>
                     </div>
-                    <a href="administrarMedicos.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="administrarUsers.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
                 </div>
 
                 <div class="card w-75 mx-auto small-box pd25">
                   <div class="card-body">
-                    <h5 class="card-title mg6 p-25">ADMINISTRAR PSICOLOGOS</h5>
-                    <p class="card-text text-muted">Ingrese aquí para administrar todos los psicologos agregados.</p>
+                    <h5 class="card-title mg6 p-25">ADMINISTRAR PSICÓLOGOS</h5>
+                    <p class="card-text text-muted">Ingrese aquí para administrar todos los psicólogos agregados.</p>
                     <div class="icon">
                       <i class="ion ion-grid"></i>
                     </div>
@@ -159,42 +152,37 @@
             }else if(isset($_SESSION['Logueado']) && isset($_SESSION['privilegio']) && ($_SESSION['Logueado'] === true) && ($_SESSION['privilegio'] == 0)){
               //admin master
               echo '
-                <div class="row">
-                  <div class="col-lg-6 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-success">
-                      <div class="inner">
-                        <h3 class="h3-adminbody">Administrar Administradores</h3>
-                      </div>
+
+                  <div class="card w-75 mx-auto small-box pd25 ">
+                    <div class="card-body">
+                      <h5 class="card-title mg6 p-25">ADMINISTRAR ADMINISTRADORES</h5>
+                      <p class="card-text text-muted">Ingrese aquí para administrar todos los administradores.</p>
                       <div class="icon">
-                        <i class="ion ion-calendar"></i>
+                          <i class="ion ion-grid"></i>
                       </div>
-                      <a href="#" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                      <a href="administrarAdmins.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
+                </div>
+
+                <div class="card w-75 mx-auto small-box pd25">
+                  <div class="card-body">
+                    <h5 class="card-title mg6 p-25">ADMINISTRAR PSICÓLOGOS</h5>
+                    <p class="card-text text-muted">Ingrese aquí para administrar todos los psicólogos agregados.</p>
+                    <div class="icon">
+                      <i class="ion ion-grid"></i>
+                    </div>
+                    <a href="administrarMedicos.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
-                  <div class="col-lg-6 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-info">
-                      <div class="inner">
-                        <h3 class="h3-adminbody">Administrar Usuarios</h3>
-                      </div>
-                      <div class="icon">
-                        <i class="ion ion-calendar"></i>
-                      </div>
-                      <a href="administrarMedicos.php" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+
+                <div class="card w-75 mx-auto small-box pd25">
+                  <div class="card-body">
+                    <h5 class="card-title mg6 p-25">ADMINISTRAR USUARIOS</h5>
+                    <p class="card-text text-muted">Ingrese aquí para administrar todos los usuarios agregados.</p>
+                    <div class="icon">
+                      <i class="ion ion-grid"></i>
                     </div>
-                  </div>
-                  <div class="col-lg-6 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-warning">
-                      <div class="inner">
-                        <h3 class="h3-adminbody">Administrar Psicólogos</h3>
-                      </div>
-                      <div class="icon">
-                        <i class="ion ion-calendar"></i>
-                      </div>
-                      <a href="administrarMedicos.php" class="small-box-footer">Más información <i class="fas fa-arrow-circle-right"></i></a>
-                    </div>
+                    <a href="administrarUsers.php" class="small-box-footer btn btn-primary wd100">Más información <i class="fas fa-arrow-circle-right"></i></a>
                   </div>
                 </div>
               ';
